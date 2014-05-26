ee98024637b4c6e0a43d49bbf62af895f15da8e1
Valerino Wijaya <valerino@gmail.com>
2014-05-26 08:29:23 +0700

    Initial Commit

---
+++ User Data Challenge/VWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWViewController.h
+//  User Data Challenge
+//
+//  Created by Valerino on 5/26/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWViewController : UIViewController
+
+@end
---
+++ User Data ChallengeTests/User_Data_ChallengeTests.m
@@ -0,0 +1,34 @@
+//
+//  User_Data_ChallengeTests.m
+//  User Data ChallengeTests
+//
+//  Created by Valerino on 5/26/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface User_Data_ChallengeTests : XCTestCase
+
+@end
+
+@implementation User_Data_ChallengeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
